31e29c98ce5a9dc01e5eb571c68881626dc4a04e
Holyvert <96556635+Holyvert@users.noreply.github.com>
2023-09-26 10:28:36 -0400

    initial services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: LandingComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-  
+
+
  }
