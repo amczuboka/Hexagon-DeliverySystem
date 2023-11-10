56b01cbd622864aee950dfdb9b08f529e00f73f4
Nicholas Piperni <npiperni@hotmail.ca>
2023-11-10 12:46:59 -0500

    Fixed up review component to dynamically generate rating
    #33

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faStar as EmptyStar } from '@fortawesome/free-regular-svg-icons';
 import { faStar as SolidStar} from '@fortawesome/free-solid-svg-icons';
+import { Review } from 'src/app/modules/delivery.models';
 
 @Component({
   selector: 'app-review',
@@ -8,6 +9,16 @@ import { faStar as SolidStar} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent {
+  @Input() review!: Review;
   reviewStarEmpty = EmptyStar;
   reviewStarSolid = SolidStar;
+
+  getStarArr(): boolean[] {
+    let stars: boolean[] = Array(5).fill({});
+    for(let i = 0; i < stars.length; i++) {
+      stars[i] = i + 1 <= this.review.stars
+    }
+    return stars;
+  }
+
 }
