1f69544963dc0d99ad9bedf42cefc2e86b33f009
KarinaSandur <karinasd07@hotmail.com>
2023-11-11 18:06:24 -0500

    Added form

--- src/app/pages/request-delivery-quotation/request-delivery-quotation.component.ts
+++ src/app/pages/request-delivery-quotation/request-delivery-quotation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-request-delivery-quotation',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./request-delivery-quotation.component.scss']
 })
 export class RequestDeliveryQuotationComponent {
+  deliverydetailsForm!: FormGroup<any>;
 
 }
