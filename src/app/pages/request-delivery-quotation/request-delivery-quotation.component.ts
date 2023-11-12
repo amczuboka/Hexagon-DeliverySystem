8393a3e54396237dfaf9664619c3a52bcf90d8f4
KarinaSandur <karinasd07@hotmail.com>
2023-11-11 19:21:09 -0500

    Added responsiveness to form

--- src/app/pages/request-delivery-quotation/request-delivery-quotation.component.ts
+++ src/app/pages/request-delivery-quotation/request-delivery-quotation.component.ts
@@ -1,5 +1,15 @@
+//import { Component } from '@angular/core';
+//import { FormGroup } from '@angular/forms';
+
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-request-delivery-quotation',
@@ -7,6 +17,18 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./request-delivery-quotation.component.scss']
 })
 export class RequestDeliveryQuotationComponent {
-  deliverydetailsForm!: FormGroup<any>;
+  deliveryDetailsForm!: FormGroup<any>;
+  //matcher: ErrorStateMatcher | undefined;
+
+  constructor(
+    private form_builder: FormBuilder
+  ){}
+
+  ngOnInit(): void{
+    this.deliveryDetailsForm = this.form_builder.group({
+      departLocation: ['', [Validators.required]],
+      destination: ['', [Validators.required]]
+    })
+  }
 
 }
