859e846880c0c8b96857d682b7208533915fc78f
amczuboka <94947740+amczuboka@users.noreply.github.com>
2023-11-28 22:32:45 -0500

    Display new items (#81)
    * Implement displaying items
    
    Implement displaying items when user adds a new item
    
    * Update Item interface
    
    Update item interface and change delivery component to use the right interface
    
    * Added button to remove Item from delivery request
    
    ---------
    
    Co-authored-by: Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
    Co-authored-by: Holyvert <96556635+Holyvert@users.noreply.github.com>

--- src/app/components/add-item-dialog/add-item-dialog.component.ts
+++ src/app/components/add-item-dialog/add-item-dialog.component.ts
@@ -20,21 +20,20 @@ export class AddItemDialogComponent implements OnInit {
 
   ngOnInit(): void {
     this.newDeliveryItem = this.form_builder.group({
-      itemDescription: ['', [Validators.required]],
-      itemWeight: ['', [Validators.required]],
-      itemSize: ['', [Validators.required]],
-      qty: ['', [Validators.required]]
+      Name: ['', [Validators.required]],
+      Weight: ['', [Validators.required]],
+      Size: ['', [Validators.required]],
+      Quantity: ['', [Validators.required]]
     });
     
 
   }
 
   saveItem(){
-    const {valid} = this.newDeliveryItem;
-    if (valid){
-      this.data.dialogRef.close();
+    const { valid, value } = this.newDeliveryItem;
+    if (valid) {
+      this.dialogRef.close(value); // Pass the new item data when closing the dialog
     }
-    //save item to delivery
   }
 
 
