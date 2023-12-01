9d51dc41b98b8f939fe04a775a7dfc9a35332b8b
KarinaSandur <karinasd07@hotmail.com>
2023-11-30 20:54:57 -0500

    Backend to save quotation

--- src/app/components/order-summary-dialog/order-summary-dialog.component.ts
+++ src/app/components/order-summary-dialog/order-summary-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { DeliveryService } from 'src/app/services/delivery.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-order-summary-dialog',
@@ -9,17 +11,24 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class OrderSummaryDialogComponent {
   deliveryObj: any;
-
+  //storageService: any;
+  
   constructor(
     private router: Router,
     private dialogRef: MatDialogRef<OrderSummaryDialogComponent>,
+    private deliveryService: DeliveryService,
+    private storageService: StorageService,
+    
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.deliveryObj = data;
-  }
+      this.deliveryObj = data;
+
+    }
 
   saveQuotation() {
     //save quotation
+    this.deliveryService.addDelivery(this.deliveryObj);
+    this.storageService.sendNotification('Delivery has been saved as quotation!');
   }
 
   proceedToPayment() {
